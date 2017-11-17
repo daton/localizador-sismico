52981506f31badf52ee28717a6291d7f3b66d89f
como-quesito <celiesita1951>
2017-11-17 17:17:27 -0600

    cuarto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppRoutingModule } from './app-routing.module';
 import { DxMapModule, DxSelectBoxModule } from 'devextreme-angular';
+import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 
 
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxMapModule,DxSelectBoxModule
+    DxMapModule,DxSelectBoxModule,
+    ClarityModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]

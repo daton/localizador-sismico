ff3e2cfae3ce5b7da602a73bcd705844f0f1a29c
como-quesito <celiesita1951>
2017-11-17 16:48:40 -0600

    segundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxMapComponent } from 'devextreme-angular';
+import { MapSetting, Service } from './service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[Service]
 })
 export class AppComponent {
-  title = 'app';
+  mapTypes: MapSetting[];
+  mapProviders: MapSetting[];  
+
+  constructor(service: Service) {
+    this.mapTypes = service.getMapTypes();
+    this.mapProviders = service.getMapProviders();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppRoutingModule } from './app-routing.module';
-
+import { DxMapModule, DxSelectBoxModule } from 'devextreme-angular';
 import { AppComponent } from './app.component';
 
 
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxMapModule,DxSelectBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
